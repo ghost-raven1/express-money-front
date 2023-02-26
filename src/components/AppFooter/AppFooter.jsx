a15f1b206f8cae789b444a5bf90915bf13d87671
ghost-raven1 <sonofagronom@gmail.com>
2023-02-26 19:00:05 +0700

    added update

--- src/components/AppFooter/AppFooter.jsx
+++ src/components/AppFooter/AppFooter.jsx
@@ -2,6 +2,8 @@ import './AppFooter.scss'
 import AppFooterSearchBlock from "./components/AppFooterSearchBlock/AppFooterSearchBlock.jsx";
 import AppFooterNavPanel from "./components/AppFooterSearchBlock/components/AppFooterNavPanel/AppFooterNavPanel.jsx";
 import AppFooterInfoLinks from "./components/AppFooterSearchBlock/components/AppFooterInfoLinks/AppFooterInfoLinks.jsx";
+import AppFooterCopyrights
+    from "./components/AppFooterSearchBlock/components/AppFooterCopyrights/AppFooterCopyrights.jsx";
 const AppFooter = () => {
     return (
         <div className="footer">
@@ -10,6 +12,7 @@ const AppFooter = () => {
                 <AppFooterNavPanel />
                 <div className="footer-inner__delimiter" />
                 <AppFooterInfoLinks />
+                <AppFooterCopyrights />
             </div>
         </div>
     )
---
+++ src/components/AppFooter/components/AppFooterSearchBlock/components/AppFooterCopyrights/AppFooterCopyrights.jsx
@@ -0,0 +1,31 @@
+import './AppFooterCopyrights.scss';
+const AppFooterCopyrights = () => {
+    return (
+        <div className="footer-copyrights">
+            <div className="footer-copyrights__first">
+                © 2013–2023, ООО МФК <br />
+                «ЭкспрессДеньги». Все права защищены.
+            </div>
+            <div className="footer-copyrights__second">
+                «Займы.рф» является торговой маркой <br />
+                ООО МФК «ЭкспрессДеньги»
+            </div>
+            <div className="footer-copyrights__third">
+                Общество с ограниченной <br />
+                ответственностью микрофинансовая <br />
+                компания "ЭкспрессДеньги" (ОГРН <br />
+                1101215004593, ИНН 1215150103), адрес <br />
+                места нахождения: 424000, Республика <br />
+                Марий Эл, г. Йошкар-Ола, бульвар <br />
+                Чавайна, д. 12, корпус А, <br />
+                регистрационный номер записи в <br />
+                государственном реестре <br />
+                микрофинансовых организаций за <br />
+                номером 2120512002049. Дата внесения в <br />
+                реестр 06 сентября 2012 года. <br />
+            </div>
+        </div>
+    )
+}
+
+export default AppFooterCopyrights;
