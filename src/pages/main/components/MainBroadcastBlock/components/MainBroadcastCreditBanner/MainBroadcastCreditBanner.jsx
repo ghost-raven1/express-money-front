86ba6ca8feea0c4a8564c4aa5e5fff55b6071939
ghost-raven1 <sonofagronom@gmail.com>
2023-03-06 14:11:37 +0700

    added update

--- src/pages/main/components/MainBroadcastBlock/components/MainBroadcastCreditBanner/MainBroadcastCreditBanner.jsx
+++ src/pages/main/components/MainBroadcastBlock/components/MainBroadcastCreditBanner/MainBroadcastCreditBanner.jsx
@@ -1,9 +1,14 @@
-import {mainBroadcastCreditBanner} from "../../../../../../utils/images.js";
+import { mainBroadcastCreditBanner } from "../../../../../../utils/images.js";
+import "./MainBroadcastCreditBanner.scss";
 
 const MainBroadcastCreditBanner = () => {
-    return (
-        <img src={mainBroadcastCreditBanner} alt="" style={{width: '100%'}} />
-    )
-}
+  return (
+    <img
+      src={mainBroadcastCreditBanner}
+      className="broadcast-credit-banner__img"
+      alt=""
+    />
+  );
+};
 
 export default MainBroadcastCreditBanner;
