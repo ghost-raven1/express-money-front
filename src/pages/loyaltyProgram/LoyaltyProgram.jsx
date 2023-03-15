b7430fe2ad960c8e1edc6cf6d02ad867fc66ff41
ghost-raven1 <sonofagronom@gmail.com>
2023-03-15 17:37:34 +0700

    added LoyaltyProgramPage

--- src/pages/loyaltyProgram/LoyaltyProgram.jsx
+++ src/pages/loyaltyProgram/LoyaltyProgram.jsx
@@ -1,13 +1,40 @@
 import "./LoyaltyProgram.scss";
 import AppHeadSection from "../../components/AppHeadSection/AppHeadSection.jsx";
-import mainTariffsBlockDelimiter from 'src/assets/main/Delimiters/Delimiter.svg';
+import mainTariffsBlockDelimiter from "src/assets/main/Delimiters/Delimiter.svg";
+import AppButton from "../../components/AppButton/AppButton.jsx";
+import SvgSelector from "../../components/SvgSelector/SvgSelector.jsx";
 const loyaltyProgram = () => {
   return (
     <div className="loyalty-program">
       <AppHeadSection
         title="Программа лояльности"
         img={mainTariffsBlockDelimiter}
+        desc="Наши постоянные клиенты получают наилучшие условия"
       />
+      <div className="loyalty-program-container">
+        <img
+          className="loyalty-program-container-left"
+          src="src/assets/loyaltyProgram/leftImg.svg"
+          alt=""
+        />
+        <div className="loyalty-program-container-right">
+            <div className="loyalty-program-container-right-form">
+                <div className="loyalty-program-container-right-form__title">
+                    Чтобы воспользоваться партнерской <br />
+                    программой Вам необходимо перейти <br />
+                    в личный кабинет
+                </div>
+                <div className="main__button-border main__button-border_grey">
+                    <a href="https://platform.expressmoney.com/loans/" target='_blank'>
+                        <AppButton mode="blue">
+                            ПЕРЕЙТИ В ЛИЧНЫЙ КАБИНЕТ <SvgSelector id="arrow-in-round" />
+                            {/* TODO Поменять иконку*/}
+                        </AppButton>
+                    </a>
+                </div>
+            </div>
+        </div>
+      </div>
     </div>
   );
 };
