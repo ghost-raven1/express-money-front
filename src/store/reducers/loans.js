53b4b640b433d903bfd9735e694986510d7b56d6
ghost-raven1 <sonofagronom@gmail.com>
2023-04-03 10:55:50 +0700

    added responses

--- src/store/reducers/loans.js
+++ src/store/reducers/loans.js
@@ -4,10 +4,72 @@ import api from "../api.js";
 // TODO: Дописать запросы
 
 const loansSlice = createSlice({
-    name: "loans",
-    initialState: {},
-    reducers: {}
-})
+  name: "loans",
+  initialState: {},
+  reducers: {},
+});
+
+export const createLoanAsync =
+  ({ data }) =>
+  async (dispatch) => {
+    await api
+      .post("/loans/loan", data)
+      .then((r) => {
+        console.log(`Создание займа ==> успешно`);
+      })
+      .catch((e) => {
+        console.error(`Создание займа ==> ошибка`, e?.response);
+      });
+  };
+
+export const getLoanAsync =
+  ({ id }) =>
+  async (dispatch) => {
+    await api
+      .get(`/loans/loan/${id}`)
+      .then((r) => {
+        console.log(`Получение займа ${id} ==> успешно`);
+      })
+      .catch((e) => {
+        console.error(`Получение займа ${id} ==> ошибка`, e?.response);
+      });
+  };
+
+export const loanPaymentAsync =
+  ({ data }) =>
+  async (dispatch) => {
+    await api
+      .post(`/loans/loan_payment`, data)
+      .then((r) => {
+        console.log(
+          `Оплата займа ${data?.loan}, id карты: ${data?.bank_card_id} ==> успешно`
+        );
+      })
+      .catch((e) => {
+        console.error(
+          `Оплата займа ${data?.loan}, id карты: ${data?.bank_card_id} ==> ошибка`,
+          e?.response
+        );
+      });
+  };
+
+export const partialPaymentAsync =
+  ({ data }) =>
+  async (dispatch) => {
+    await api
+      .post(`/loans/partials_payment`, data)
+      .then((r) => {
+        console.log(
+          `Частичная оплата займа ${data?.amount}, id карты: ${data?.bank_card_id} ==> успешно`
+        );
+      })
+      .catch((e) => {
+        console.error(
+          `Частичная оплата займа ${data?.amount}, id карты: ${data?.bank_card_id} ==> ошибка`,
+          e?.response
+        );
+      });
+  };
 
 export const {} = loansSlice.actions;
 
--- src/store/reducers/storage.js
+++ src/store/reducers/storage.js
@@ -22,37 +22,48 @@ export const uploadPassportAsync =
       });
   };
 
-export const uploadFileAsync = ({ data }) => async (dispatch) => {
-    await api.post(`/storage/upload_file`, data)
-        .then((r) => {
-            console.log("Загрузка файла ==> успешно")
-        })
-        .catch((e) => {
-            console.error("Загрузка файла ==> ошибка", e?.response)
-        })
-}
+export const uploadFileAsync =
+  ({ data }) =>
+  async (dispatch) => {
+    await api
+      .post(`/storage/upload_file`, data)
+      .then((r) => {
+        console.log("Загрузка файла ==> успешно");
+      })
+      .catch((e) => {
+        console.error("Загрузка файла ==> ошибка", e?.response);
+      });
+  };
 
-export const userFileDownloadAsync = ({ name }) => async (dispatch) => {
-    await api.get(`/storage/user_file/${name}/download`)
-        .then((r) => {
-            console.log(`Скачивание файла ${name} ==> успешно`)
-        })
-        .catch((e) => {
-            console.error(`Скачивание файла ${name} ==> ошибка`, e?.response)
-        })
-}
+export const userFileDownloadAsync =
+  ({ name }) =>
+  async (dispatch) => {
+    await api
+      .get(`/storage/user_file/${name}/download`)
+      .then((r) => {
+        console.log(`Скачивание файла ${name} ==> успешно`);
+      })
+      .catch((e) => {
+        console.error(`Скачивание файла ${name} ==> ошибка`, e?.response);
+      });
+  };
 
-export const getUserFileAsync = ({ name }) => async (dispatch) => {
-    await api.get(`/storage/user_file/${name}`)
-        .then((r) => {
-            console.log(`Получение файла ${name} ==> успешно`)
-        })
-        .catch((e) => {
-            console.error(`Получение файла ${name} ==> ошибка`, e?.response)
-        })
-}
+export const getUserFileAsync =
+  ({ name }) =>
+  async (dispatch) => {
+    await api
+      .get(`/storage/user_file/${name}`)
+      .then((r) => {
+        console.log(`Получение файла ${name} ==> успешно`);
+      })
+      .catch((e) => {
+        console.error(`Получение файла ${name} ==> ошибка`, e?.response);
+      });
+  };
 
-export const getUserFileListAsync = ({params}) => async (dispatch) => {
+export const getUserFileListAsync =
+  ({ params }) =>
+  async (dispatch) => {
     await api
       .get(`/storage/user_file`, { params: params })
       .then((r) => {
@@ -61,7 +72,39 @@ export const getUserFileListAsync = ({params}) => async (dispatch) => {
       .catch((e) => {
         console.error(`Получение списка файлов ==> ошибка`, e?.response);
       });
-}
+  };
+
+export const getPublicFileAsync =
+  ({ name }) =>
+  async () => {
+    await api
+      .get(`/storage/public_file/${name}`)
+      .then((r) => {
+        console.log(`Получение публичного файла ${name} ==> успешно`);
+      })
+      .catch((e) => {
+        console.error(
+          `Получение публичного файла ${name} ==> ошибка`,
+          e?.response
+        );
+      });
+  };
+
+export const getPublicFileListAsync =
+  ({ params }) =>
+  async (dispatch) => {
+    await api
+      .get(`/storage/public_file`, { params: params })
+      .then((r) => {
+        console.log(`Получение списка публичных файлов ==> успешно`);
+      })
+      .catch((e) => {
+        console.error(
+          `Получение списка публичных файлов ==> ошибка`,
+          e?.response
+        );
+      });
+  };
 
 export const {} = storageSlice.actions;
 
