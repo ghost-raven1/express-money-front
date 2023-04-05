d3017562f6636ba2fb419d180e1043dcad91e7c9
ghost-raven1 <sonofagronom@gmail.com>
2023-04-05 18:48:54 +0700

    added fixes AppPage logic

--- src/components/AmountOfCreditForm/AmountOfCreditForm.jsx
+++ src/components/AmountOfCreditForm/AmountOfCreditForm.jsx
@@ -1,48 +1,95 @@
 import "./AmountOfCreditForm.scss";
-import { useState } from "react";
+import {useEffect, useLayoutEffect, useState} from "react";
 import AmountOfCreditFormRange from "./components/AmountOfCreditFormRange/AmountOfCreditFormRange.jsx";
 import AppSelect from "../AppSelect/AppSelect.jsx";
 import AppButton from "../AppButton/AppButton.jsx";
 import SvgSelector from "../SvgSelector/SvgSelector.jsx";
+import { useDispatch, useSelector } from "react-redux";
+import { getDefaultProductAsync } from "../../store/reducers/products.js";
 const AmountOfCreditForm = () => {
-  const [amount, setAmount] = useState(50000);
-  const [days] = useState([{ id: "1", name: "1 день" }]);
+  // TODO: Проверить логику
+  const priceLabels = ["1000 руб", "50 000 руб", "100 000 руб"];
+  const dispatch = useDispatch();
+  const { freePeriod, interests } = useSelector(
+    (state) => state.productsReducer
+  );
+  const [days] = useState([
+    { id: "1", name: "1 день" },
+    { id: "7", name: "7 дней" },
+    { id: "30", name: "30 дней" },
+  ]);
+  const [freePeriodData, setFreePeriodData] = useState(freePeriod);
+  const [interestsData, setInterestsData] = useState(interests);
+  const [calcAmount, setCalcAmount] = useState(50000); // id_amount_requested
+  const [calcPeriod, setCalcPeriod] = useState(1); // id_period_requested
+  // const [calcPromocode, setPromocode] = useState(); // id_promocode_code
+
+  function calcInterests() {
+    const i =
+      ((calcAmount * interests) / 100) *
+      (calcPeriod - freePeriodData);
+    console.log("interests", i);
+    setInterestsData(i);
+  }
+
+  // get_promocode_product(_event):
+  //     calc_promocode.value = calc_promocode.value.upper()
+  //     if len(calc_promocode.value) > 4:
+  //         url = f"{API}/promocodes/promocode/{calc_promocode.value}"
+  //         ajax.get(url, oncomplete=set_promocode_product, mode="json")
+  //         change_table(None)
+
+  useLayoutEffect(() => {
+    dispatch(getDefaultProductAsync());
+  }, []);
+
+  useEffect(() => {
+    calcInterests();
+  }, [calcAmount, interestsData, calcPeriod, freePeriodData]);
 
   return (
     <div className="amount-of-credit-form">
       <div className="amount-of-credit-form__title">Выберите сумму</div>
       <div className="credit-form-container">
         <div className="credit-form-container__left">
-          <AmountOfCreditFormRange value={amount} setValue={setAmount} step={1000} />
+          <AmountOfCreditFormRange
+            value={+calcAmount}
+            setValue={setCalcAmount}
+            step={1000}
+          />
           <div className="credit-form-container__left-prices">
-            <div className="credit-form-container__left-prices-price">
-              1000 руб
-            </div>
-            <div className="credit-form-container__left-prices-price">
-              50 000 руб
-            </div>
-            <div className="credit-form-container__left-prices-price">
-              100 000 руб
-            </div>
+            {priceLabels?.length &&
+              priceLabels.map((label, idx) => {
+                return (
+                  <div
+                    className="credit-form-container__left-prices-price"
+                    key={idx}
+                  >
+                    {label}
+                  </div>
+                );
+              })}
           </div>
           <div className="credit-form-container__left-text">
-            К возврату 66 000 ₽
+            К возврату {calcAmount + interests} ₽
           </div>
         </div>
-        <AppSelect optionsList={days} mode="grey" />
+        <AppSelect optionsList={days} mode="grey" onInput={setCalcPeriod} />
       </div>
       <div className="main__button-border main__button-border_black">
-        <a href="https://platform.expressmoney.com/loans/" target='_blank'>
+        <a href="https://platform.expressmoney.com/loans/" target="_blank">
           <AppButton mode="black">
             ОФОРМИТЬ ЗАЯВКУ
             <SvgSelector id="arrow-in-round" />
           </AppButton>
         </a>
       </div>
-      {/*<div className="amount-of-credit-form__bottom">*/}
+      <div className="amount-of-credit-form__bottom">
       {/*  <AppCheckbox label="Карта лояльности" />*/}
-      {/*  <div className="amount-of-credit-form__bottom-percent">1% в сутки</div>*/}
-      {/*</div>*/}
+      <div className="amount-of-credit-form__bottom-percent">
+        {interestsData}% в сутки
+      </div>
+      </div>
     </div>
   );
 };
